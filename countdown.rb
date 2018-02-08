29bc08b214f1a0ae354eacf2be3335290e5d6192
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-02-08 05:36:31 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,9 +10,9 @@ end
 
 def countdown_with_sleep(number)
   until (number == 0)
-    sleep 1
     puts "#{number} SECOND(S)!"
     number -= 1
+        sleep 1
     end
     return "HAPPY NEW YEAR!"
 
